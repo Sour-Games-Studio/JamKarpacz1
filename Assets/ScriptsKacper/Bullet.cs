5bd86aa38b44e30b09b02a7139e3f7cd65d93b01
Dawleb <dawleb777@gmail.com>
2024-12-19 19:28:30 +0100

    meleedmg and immuframes done

--- Assets/ScriptsKacper/Bullet.cs
+++ Assets/ScriptsKacper/Bullet.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+    [SerializeField] public string owner = "Player";
+    [SerializeField] public float damage = 1f;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        //print tag it entered
+        //Debug.Log(other.tag+ "Entered tag");
+        if (other.CompareTag("Player")&& owner == "Enemy")
         {
-            other.GetComponent<PlayerMovement>().TakeDamagePlayer(1);
+            other.GetComponent<PlayerMovement>().TakeDamagePlayer(damage);
+            
+        }
+        if (other.CompareTag("Enemy") && owner == "Player")
+        {
+            other.GetComponent<EnemyAi>().TakeDamage(damage);
+        }
+        if (other.tag == owner)
+        {
+            return;
         }
         Destroy(this.gameObject);
        
