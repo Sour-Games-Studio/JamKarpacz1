353907b86206f60301c1fb51b474f862c89bf0a0
PLeX0 <plexstream2@gmail.com>
2024-12-19 15:25:22 +0100

    EnemyAi

---
+++ Assets/ScriptsKacper/EnemyAi.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject bullet2;
+    [SerializeField] private float attackSpeedTimer;
+    [SerializeField] private float attackSpeed = 3f;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float damage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        attackSpeedTimer = attackSpeed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.transform.position;
+        if (attackSpeedTimer > 0)
+        {
+            attackSpeedTimer -= Time.deltaTime;
+        }
+        else if (attackSpeedTimer <= 0)
+        {
+            //strzal
+            var currentBullet = Instantiate(bullet);
+            currentBullet.transform.position = this.transform.position;
+           currentBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+            attackSpeedTimer = attackSpeed;          
+        }
+        Debug.Log(attackSpeedTimer.ToString());
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //obrazenia dla gracza
+            Debug.Log("-10hp");
+        }
+    }
+
+}
