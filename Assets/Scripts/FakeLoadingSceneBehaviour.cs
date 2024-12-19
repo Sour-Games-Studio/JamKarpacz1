0086dd10d75ffe1c67908f95a581289eec966847
OgorekHD <igor10126@wp.pl>
2024-12-19 19:30:45 +0100

    loadinganim

--- Assets/Scripts/FakeLoadingSceneBehaviour.cs
+++ Assets/Scripts/FakeLoadingSceneBehaviour.cs
@@ -2,31 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class FakeLoadingSceneBehaviour : MonoBehaviour
 {
-    [SerializeField] private GameObject LoadText;
+    [SerializeField] private GameObject LoadTextObject;
     [SerializeField] private GameObject PressAnyButtonText;
+    [SerializeField] private TMP_Text LoadText;
     private bool Loaded;
 
     private void Start()
     {
+        LoadText = LoadTextObject.GetComponent<TMP_Text>();
         Invoke("FinishLoading", 4f);
+        noDots();
     }
 
     private void FinishLoading()
     {
         Loaded = true;
-        LoadText.SetActive(false);
+        LoadTextObject.SetActive(false);
         PressAnyButtonText.SetActive(true);
     }
 
     private void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && Loaded)
         {
             SceneManager.LoadScene("Main");
         }
+    }
+
+
+
+    private void noDots()
+    {
+        LoadText.text = "Loading";
+        Invoke("oneDot", 0.5f);
+    }
+
+    private void oneDot()
+    {
+        LoadText.text = "Loading.";
+        Invoke("twoDots", 0.5f);
+    }
+
+    private void twoDots()
+    {
+        LoadText.text = "Loading..";
+        Invoke("threeDots", 0.5f);
+    }
+
+    private void threeDots()
+    {
+        LoadText.text = "Loading...";
+        Invoke("noDots", 0.5f);
     }
 
 }
